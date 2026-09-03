3a478ad8d6f2a072ac42818ea4edb87e62609d61
Zakir Matloob <webdeveloper.zakir152@gmail.com>
2026-09-03 08:57:13 +0500

    Update allowedDevOrigins in next.config.ts to include new IP address for development

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
-  allowedDevOrigins: ["127.0.0.1", "localhost"],
+  allowedDevOrigins: ["127.0.0.1", "localhost", "10.204.101.220"],
 };
 
 export default nextConfig;
